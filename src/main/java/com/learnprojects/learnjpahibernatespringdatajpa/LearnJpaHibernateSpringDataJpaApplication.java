45b59084261442e573d3be149eb155d7cce2175c
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-26 14:28:13 +0200

    Created spring boot project

---
+++ src/main/java/com/learnprojects/learnjpahibernatespringdatajpa/LearnJpaHibernateSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learnprojects.learnjpahibernatespringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaHibernateSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaHibernateSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnprojects/learnjpahibernatespringdatajpa/LearnJpaHibernateSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnprojects.learnjpahibernatespringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaHibernateSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
