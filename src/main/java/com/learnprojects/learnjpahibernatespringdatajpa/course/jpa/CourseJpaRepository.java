d8300ee78d408e76627ac9a9c26a0e355eba0002
Igor Stein <70313147+becks-boo@users.noreply.github.com>
2023-09-26 16:19:11 +0200

    Created repository to use jpa with, created insert, findById and deleteById methods

---
+++ src/main/java/com/learnprojects/learnjpahibernatespringdatajpa/course/jpa/CourseJpaRepository.java
@@ -0,0 +1,27 @@
+package com.learnprojects.learnjpahibernatespringdatajpa.course.jpa;
+
+import com.learnprojects.learnjpahibernatespringdatajpa.course.Course;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class CourseJpaRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public void insert(Course course) {
+        entityManager.merge(course);
+    }
+
+    public Course findById(long id) {
+        return entityManager.find(Course.class, id);
+    }
+
+    public void deleteById(long id) {
+        Course course = entityManager.find(Course.class, id);
+        entityManager.remove(course);
+    }
+}
